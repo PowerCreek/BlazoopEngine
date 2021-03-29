a22a5a1e25adc57c73bc58c81d9a231032915589
EpicMice <micecd@gmail.com>
2021-03-28 20:37:32 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blazoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
